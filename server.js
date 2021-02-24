31b86e685baf3a38deaae35f49f0a34cbb63e4be
IiIHKIiI <tdhk96@gmail.com>
2021-02-24 14:09:54 +0700

    config to deploy to heroku

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 let express = require('express'),
-   path = require('path'),
-   mongoose = require('mongoose'),
-   cors = require('cors'),
-   bodyParser = require('body-parser'),
-   dbConfig = require('../database/db');
+  path = require('path'),
+  mongoose = require('mongoose'),
+  cors = require('cors'),
+  bodyParser = require('body-parser'),
+  dbConfig = require('../database/db');
 createError = require('http-errors');
 // Connecting with mongo db
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.db, {
-   useNewUrlParser: true,
-   useFindAndModify: false,
+  useNewUrlParser: true,
+  useFindAndModify: false,
 }).then(() => {
-   console.log('Database sucessfully connected')
+  console.log('Database sucessfully connected')
 },
-   error => {
-      console.log('Database could not connected: ' + error)
-   }
+  error => {
+    console.log('Database could not connected: ' + error)
+  }
 )
 
 // Setting up port with express js
@@ -32,18 +32,22 @@ const webhostingRoute = require('../routes/webhosting.route')
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-   extended: false
+  extended: false
 }));
 app.use(cors());
 app.use(function (req, res, next) {
-   //Enabling CORS
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-   next();
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
 });
-app.use(express.static(path.join(__dirname, 'dist/JIRA')));
-app.use('/', express.static(path.join(__dirname, 'dist/JIRA')));
+app.use(express.static(path.join(__dirname, 'dist/jira-agg')));
+app.use('/', express.static(path.join(__dirname, 'dist/jira-agg')));
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/jira-agg/index.html'));
+});
 // Set up URL
 app.use('/api/units/', unitRoute)
 app.use('/api/users/', userRoute)
@@ -56,19 +60,19 @@ app.use('/api/domain/', domainRoute)
 app.use('/api/webhosting/', webhostingRoute)
 
 // Create port
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
-   console.log('Connected to port ' + port)
+  console.log('Connected to port ' + port)
 })
 
 // Find 404 and hand over to error handler
 app.use((req, res, next) => {
-   next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-   console.error(err.message); // Log error message in our server's console
-   if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-   res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+  console.error(err.message); // Log error message in our server's console
+  if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+  res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
